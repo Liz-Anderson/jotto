5899caa96faf89293ef22c944a8a802a8692a449
liz <cleanbean@gmail.com>
2024-01-26 16:40:26 -0800

    made inputs accept one letter and automatically tab to next input

--- site.js
+++ site.js
@@ -5,6 +5,12 @@ const app = Vue.createApp({
             alphabet: [
                 {name: 'A', pick: 0}, {name: 'B', pick: 0}, {name: 'C', pick: 0}, {name: 'D', pick: 0}, {name: 'E', pick: 0}, {name: 'F', pick: 0}, {name: 'G', pick: 0}, {name: 'H', pick: 0}, {name: 'I', pick: 0}, {name: 'J', pick: 0}, {name: 'K', pick: 0}, {name: 'L', pick: 0}, {name: 'M', pick: 0}, {name: 'N', pick: 0}, {name: 'O', pick: 0}, {name: 'P', pick: 0}, {name: 'Q', pick: 0}, {name: 'R', pick: 0}, {name: 'S', pick: 0}, {name: 'T', pick: 0}, {name: 'U', pick: 0}, {name: 'V', pick: 0}, {name: 'W', pick: 0}, {name: 'X', pick: 0}, {name: 'Y', pick: 0}, {name: 'Z', pick: 0}
             ],
+            input1: '',
+            input2: '',
+            input3: '',
+            input4: '',
+            input5: '',
+            
         }
 
     },
@@ -15,7 +21,13 @@ const app = Vue.createApp({
             } else {
                 letter.pick = 0
             }
-        }
+        },
+        enter(){
+            console.log("I entered a letter")
+        },
+        next(e) {
+            e.target?.nextSibling?.focus();
+          },
 
         
     }
